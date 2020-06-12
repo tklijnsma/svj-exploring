a746f25e96ddc7ec076ef9d1dd1adb4cba473a56
Thomas Klijnsma <klijnsma@fnal.gov>
2020-06-12 13:36:42 -0500

    added roc curves

--- datasets.py
+++ datasets.py
@@ -125,9 +125,10 @@ def get_trigger_titles():
     return TRIGGER_TITLES
 
 class Dataset(object):
-    def __init__(self, name, rootfiles, treename='TreeMaker2/PreSelection', is_signal=False):
+    def __init__(self, name, rootfiles, treename='TreeMaker2/PreSelection', is_signal=False, bkgname=None):
         super().__init__()
         self.name = name
+        self.bkgname = bkgname
         self.rootfiles = rootfiles
         self.treename = treename
         self.shortname = self.name[:20]
@@ -237,13 +238,68 @@ class Dataset(object):
 # Shortcuts to make the datasets
 
 def init_ttjets_datasets():
-    return [ Dataset(name, get_bkg_rootfiles(name)) for name in ttjets ]
+    return [ Dataset(name, get_bkg_rootfiles(name), bkgname='ttjets') for name in ttjets ]
 
 def init_qcd_datasets():
-    return [ Dataset(name, get_bkg_rootfiles(name)) for name in qcd ]
+    return [ Dataset(name, get_bkg_rootfiles(name), bkgname='qcd') for name in qcd ]
 
 def init_wjets_datasets():
-    return [ Dataset(name, get_bkg_rootfiles(name)) for name in wjets ]
+    return [ Dataset(name, get_bkg_rootfiles(name), bkgname='wjets') for name in wjets ]
 
 def init_zjets_datasets():
-    return [ Dataset(name, get_bkg_rootfiles(name)) for name in zjets ]
+    return [ Dataset(name, get_bkg_rootfiles(name), bkgname='zjets') for name in zjets ]
+
+def init_bkgs(make_cache=False, cache_branches=None):
+    ttjets = init_ttjets_datasets()
+    qcd = init_qcd_datasets()
+    wjets = init_wjets_datasets()
+    zjets = init_zjets_datasets()
+    if make_cache:
+        cache_branches = [b'JetsAK15', b'JetsAK15_softDropMass', b'TriggerPass', b'MET', b'METPhi']
+        # For qcd and ttjets 1 file contain more than enough events
+        for dataset in ttjets + qcd:
+            dataset.cache_branches(cache_branches, n_files=1)
+        # For wjets and zjets we need a little more files for a reasonable test set
+        for dataset in wjets:
+            dataset.cache_branches(cache_branches, n_files=2)
+        for dataset in zjets:
+            dataset.cache_branches(cache_branches, n_files=4)
+    return ttjets, qcd, wjets, zjets
+
+def iterate_bkg(bkgs, use_cache=True, branches=None, **kwargs):
+    """
+    Yields arrays from lists of datasets
+    'arrays' is like the TTree per file
+    Expects: bkgs = [ [ Dataset, Dataset, ... ], ... ]
+    """
+    if branches is None: branches = [b'JetsAK15_softDropMass', b'TriggerPass']
+    if use_cache: logger.warning('Using cached data (limited)')
+    for bkg in bkgs:
+        for dataset in bkg:
+            arrays_iterator = dataset.cache if use_cache else dataset.iterate_branches(branches=branches, **kwargs)
+            # Weight takes into account the cross section and the number of events
+            weight = dataset.get_weight(use_cache=use_cache)
+            for arrays in arrays_iterator:
+                yield arrays, weight, dataset
+
+
+def iterate_sig(datasets, use_cache=True, branches=None, **kwargs):
+    """
+    Yields arrays from a list of datasets
+    'arrays' is like the TTree per file
+    Expects: datasets = [ Dataset, Dataset, ... ]
+    """
+    if branches is None: branches = [b'JetsAK15_softDropMass', b'TriggerPass']
+    if use_cache: logger.warning('Using cached data (limited)')
+    for signal in datasets:
+        arrays_iterator = signal.cache if use_cache else signal.iterate_branches(branches=branches, **kwargs)
+        weight = signal.get_weight(use_cache=use_cache)
+        for arrays in arrays_iterator:
+            yield arrays, weight, signal
+
+def numentries(arrays):
+    """
+    Counts the number of entries in a typical arrays from a ROOT file,
+    by looking at the length of the first key
+    """
+    return arrays[list(arrays.keys())[0]].shape[0]
